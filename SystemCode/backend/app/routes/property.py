0d7d274acf058d1af01f353b874e0a03940b15f4
Taro <1481888028@qq.com>
2025-10-24 23:11:45 +0800

    去掉mock代码

--- SystemCode/backend/app/routes/property.py
+++ SystemCode/backend/app/routes/property.py
@@ -6,16 +6,13 @@ import openai
 from app.dependencies import get_async_session, get_async_openai_client
 from app.dependencies import get_async_openai_client
 from app.models import EnquiryForm, EnquiryNL, PropertyLocation, RecommendationResponse
-from app.utils import mock_data, mock_map
 from app.handlers import property_handler
 
 
 router = APIRouter(prefix="/api/v1/properties", tags=["properties"])
 
-MOCK_MODE = False
 
-
-# 提交问卷表单
+# Submit the questionnaire form
 @router.post("/submit-form", response_model=RecommendationResponse, status_code=status.HTTP_201_CREATED)
 async def submit_form(
     *,
@@ -23,13 +20,11 @@ async def submit_form(
     client: openai.AsyncOpenAI = Depends(get_async_openai_client),
     enquiry: EnquiryForm
 ):
-    if MOCK_MODE:
-        return mock_data.create_mock_response()
-    else:
-        return await property_handler.submit_form_handler(db=db, client=client, enquiry=enquiry)
+
+    return await property_handler.submit_form_handler(db=db, client=client, enquiry=enquiry)
 
 
-# 提交文字表单
+# Submit natural language description
 @router.post("/submit-description", response_model=RecommendationResponse, status_code=status.HTTP_201_CREATED)
 async def submit_description(
     *,
@@ -37,13 +32,11 @@ async def submit_description(
     client: openai.AsyncOpenAI = Depends(get_async_openai_client),
     enquiry: EnquiryNL
 ):
-    if MOCK_MODE:
-        return mock_data.create_mock_response()
-    else:
-        return await property_handler.submit_description_handler(db=db, client=client, enquiry=enquiry)
+
+    return await property_handler.submit_description_handler(db=db, client=client, enquiry=enquiry)
 
 
-# 获取无表单房源推荐列表
+# Get a list of recommended properties
 @router.get("/recommendation-no-submit", response_model=RecommendationResponse, status_code=status.HTTP_200_OK)
 async def recommendation_no_submit(
     *,
@@ -52,13 +45,10 @@ async def recommendation_no_submit(
     return RecommendationResponse(properties=[])
 
 
-# 获取房源地图
+# Get the map location of a property
 @router.post("/map", response_class=HTMLResponse, status_code=status.HTTP_201_CREATED)
 async def map(
     *,
     location: PropertyLocation
 ):
-    if MOCK_MODE:
-        return HTMLResponse(content=mock_map.create_mock_response())
-    else:
-        return await property_handler.map_handler(location=location) 
+    return await property_handler.map_handler(location=location) 

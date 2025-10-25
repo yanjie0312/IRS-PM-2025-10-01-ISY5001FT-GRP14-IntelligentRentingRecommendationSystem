401198ccc56a7c610ed4868222067645ab1e259c
yanjie0312 <e1349375@u.nus.edu>
2025-10-25 21:00:13 +0800

    Update next.config.mjs

--- SystemCode/frontend/next.config.mjs
+++ SystemCode/frontend/next.config.mjs
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://irrsbackend-234550193243.asia-southeast1.run.app/api/:path*',
+      },
+    ];
+  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;

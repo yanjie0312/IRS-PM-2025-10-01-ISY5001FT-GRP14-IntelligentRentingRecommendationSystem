92e4973d3815d73bd2e2d738f22e71afe0fd496b
Taro <1481888028@qq.com>
2025-10-25 11:03:41 +0800

    配置优化

--- SystemCode/backend/app/config.py
+++ SystemCode/backend/app/config.py
@@ -8,20 +8,24 @@ class Settings(BaseSettings):
     # cloud database
     CLOUD_DB_HOST: str
     CLOUD_DB_PORT: str
-    CLOUD_DB_DATABASE: str
     CLOUD_DB_USERNAME: str
     CLOUD_DB_PASSWORD: str
+    CLOUD_DB_DATABASE: str
 
     # app database
-    APP_DB_NAME: str
-    APP_DB_USER: str
-    APP_DB_PASSWORD: str
     APP_DB_HOST: str
     APP_DB_PORT: str
+    APP_DB_USER: str
+    APP_DB_PASSWORD: str
+    APP_DB_NAME: str
 
     # pgAdmin
     PGADMIN_EMAIL: str
     PGADMIN_PASSWORD: str
+
+    # Redis
+    REDIS_HOST: str
+    REDIS_PORT: str
 
     class Config:
         env_file = ".env"
